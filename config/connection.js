36733f5e0053127516ee3eb1119d3b756484cc1e
ulas <udegertekin@gmail.com>
2018-09-20 21:40:22 -0500

    addition

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql= require("mysql");
 var connection= mysql.createConnection({
-    host: "l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    host: "localhost",
     port: 3306,
-    user: "upk9fbuu2op3zd7c",
-    password: "eh5d8fza78b05lc7",
-    database: "z8ph0rwsxj87h9ig"
+    user: "root",
+    password: "995302202",
+    database: "burgers_db"
 });
 connection.connect(function(err) {
     if (err) {
